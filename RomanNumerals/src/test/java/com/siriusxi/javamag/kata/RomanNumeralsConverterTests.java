deb9c972d765f3d8c6a520cbcc73bbdac57e5b08
Mohamed Taman <mohamed.taman@gmail.com>
2020-09-14 22:33:52 +0200

    Step #2: Setting up the TDD environment

--- RomanNumerals/src/test/java/com/siriusxi/javamag/kata/RomanNumeralsConverterTests.java
+++ RomanNumerals/src/test/java/com/siriusxi/javamag/kata/RomanNumeralsConverterTests.java
@@ -1,4 +1,12 @@
 package com.siriusxi.javamag.kata;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class RomanNumeralsConverterTests {
+    @Test
+    public void isJunitWorks(){
+        assertTrue(true);
+    }
 }
