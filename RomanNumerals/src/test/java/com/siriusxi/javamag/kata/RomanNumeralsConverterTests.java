78f4396287f71cc3b25434c140f88852f281e7c1
Mohamed Taman <mohamed.taman@gmail.com>
2020-09-15 06:25:00 +0200

    Step #3: First Refactoring - Rename Variable

--- RomanNumerals/src/test/java/com/siriusxi/javamag/kata/RomanNumeralsConverterTests.java
+++ RomanNumerals/src/test/java/com/siriusxi/javamag/kata/RomanNumeralsConverterTests.java
@@ -2,11 +2,22 @@ package com.siriusxi.javamag.kata;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RomanNumeralsConverterTests {
     @Test
     public void isJunitWorks(){
         assertTrue(true);
+    }
+
+    @Test
+    public void convertsSingleRomanDigit(){
+        int arabic = convert("I");
+        assertEquals(1, arabic);
+    }
+
+    private int convert(String i) {
+        return 1;
     }
 }
