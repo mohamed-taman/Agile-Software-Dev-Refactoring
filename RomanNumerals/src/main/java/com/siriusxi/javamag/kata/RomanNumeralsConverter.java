255b75150494fb13253f235c2b78625ad7bf88ba
Mohamed Taman <mohamed.taman@gmail.com>
2020-09-15 08:54:09 +0200

    Step #5: Adding New Single Roman Numerals Cases

--- RomanNumerals/src/main/java/com/siriusxi/javamag/kata/RomanNumeralsConverter.java
+++ RomanNumerals/src/main/java/com/siriusxi/javamag/kata/RomanNumeralsConverter.java
@@ -2,6 +2,11 @@ package com.siriusxi.javamag.kata;
 
 public class RomanNumeralsConverter {
     static int convert(String romanNumeral) {
-        return 1;
+        if (romanNumeral.equals("I"))
+            return 1;
+        if (romanNumeral.equals("V"))
+            return 5;
+        else
+            return 10;
     }
 }
--- RomanNumerals/src/test/java/com/siriusxi/javamag/kata/RomanNumeralsConverterTests.java
+++ RomanNumerals/src/test/java/com/siriusxi/javamag/kata/RomanNumeralsConverterTests.java
@@ -15,6 +15,12 @@ public class RomanNumeralsConverterTests {
     public void convertsSingleRomanDigit(){
         int arabic = RomanNumeralsConverter.convert("I");
         assertEquals(1, arabic);
+
+        arabic = RomanNumeralsConverter.convert("V");
+        assertEquals(5, arabic);
+
+        arabic = RomanNumeralsConverter.convert("X");
+        assertEquals(10, arabic);
     }
 
 }
