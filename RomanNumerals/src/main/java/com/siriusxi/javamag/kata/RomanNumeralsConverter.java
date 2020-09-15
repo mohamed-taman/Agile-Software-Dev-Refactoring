0b308aab10ae2d1e4374f9cb28247e39847d91ee
Mohamed Taman <mohamed.taman@gmail.com>
2020-09-15 16:14:52 +0200

    Step #6.3: Refactoring: Extract Method

--- RomanNumerals/src/main/java/com/siriusxi/javamag/kata/RomanNumeralsConverter.java
+++ RomanNumerals/src/main/java/com/siriusxi/javamag/kata/RomanNumeralsConverter.java
@@ -4,21 +4,7 @@ public class RomanNumeralsConverter {
 
   static int convert(String romanNumeral) {
     if (romanNumeral.length() == 1) {
-      if (romanNumeral.equals("I")) {
-        return 1;
-      } else if (romanNumeral.equals("V")) {
-        return 5;
-      } else if (romanNumeral.equals("X")) {
-        return 10;
-      } else if (romanNumeral.equals("L")) {
-        return 50;
-      } else if (romanNumeral.equals("C")) {
-        return 100;
-      } else if (romanNumeral.equals("D")) {
-        return 500;
-      } else {
-        return 1000;
-      }
+      return convertSingleDigit(romanNumeral);
     }
     if (romanNumeral.equals("II")) {
       return 2;
@@ -26,6 +12,24 @@ public class RomanNumeralsConverter {
       return 3;
     } else {
       return 6;
+    }
+  }
+
+  private static int convertSingleDigit(String romanNumeral) {
+    if (romanNumeral.equals("I")) {
+      return 1;
+    } else if (romanNumeral.equals("V")) {
+      return 5;
+    } else if (romanNumeral.equals("X")) {
+      return 10;
+    } else if (romanNumeral.equals("L")) {
+      return 50;
+    } else if (romanNumeral.equals("C")) {
+      return 100;
+    } else if (romanNumeral.equals("D")) {
+      return 500;
+    } else {
+      return 1000;
     }
   }
 }
