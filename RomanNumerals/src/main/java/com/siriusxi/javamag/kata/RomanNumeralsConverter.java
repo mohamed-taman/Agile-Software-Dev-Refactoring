d64d44cb7f2d3c6554e20f28554d207e296c48c2
Mohamed Taman <mohamed.taman@gmail.com>
2020-09-15 14:05:42 +0200

    Step #6: Continue solving the Code Kata

--- RomanNumerals/src/main/java/com/siriusxi/javamag/kata/RomanNumeralsConverter.java
+++ RomanNumerals/src/main/java/com/siriusxi/javamag/kata/RomanNumeralsConverter.java
@@ -1,12 +1,21 @@
 package com.siriusxi.javamag.kata;
 
 public class RomanNumeralsConverter {
-    static int convert(String romanNumeral) {
-        if (romanNumeral.equals("I"))
-            return 1;
-        if (romanNumeral.equals("V"))
-            return 5;
-        else
-            return 10;
+  static int convert(String romanNumeral) {
+    if (romanNumeral.equals("I")) {
+      return 1;
+    } else if (romanNumeral.equals("V")) {
+      return 5;
+    } else if (romanNumeral.equals("X")) {
+      return 10;
+    } else if (romanNumeral.equals("L")) {
+      return 50;
+    } else if (romanNumeral.equals("C")) {
+      return 100;
+    } else if (romanNumeral.equals("D")) {
+      return 500;
+    } else {
+      return 1000;
     }
+  }
 }
--- RomanNumerals/src/test/java/com/siriusxi/javamag/kata/RomanNumeralsConverterTests.java
+++ RomanNumerals/src/test/java/com/siriusxi/javamag/kata/RomanNumeralsConverterTests.java
@@ -6,16 +6,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RomanNumeralsConverterTests {
-    @Test
-    public void isJunitWorks(){
-        assertTrue(true);
-    }
+  @Test
+  public void isJunitWorks() {
+    assertTrue(true);
+  }
 
-    @Test
-    public void convertsSingleRomanDigit(){
-        assertEquals(1, RomanNumeralsConverter.convert("I"));
-        assertEquals(5, RomanNumeralsConverter.convert("V"));
-        assertEquals(10, RomanNumeralsConverter.convert("X"));
-    }
-
+  @Test
+  public void convertsSingleRomanDigit() {
+    assertEquals(1, RomanNumeralsConverter.convert("I"));
+    assertEquals(5, RomanNumeralsConverter.convert("V"));
+    assertEquals(10, RomanNumeralsConverter.convert("X"));
+    assertEquals(50, RomanNumeralsConverter.convert("L"));
+    assertEquals(100, RomanNumeralsConverter.convert("C"));
+    assertEquals(500, RomanNumeralsConverter.convert("D"));
+    assertEquals(1000, RomanNumeralsConverter.convert("M"));
+  }
 }
