6417f3ff027cba8af0452850e56b97e2b2429093
Mohamed Taman <mohamed.taman@gmail.com>
2020-09-15 06:48:45 +0200

    Step #4: micro-refactoring - Change convert() to static

--- RomanNumerals/src/test/java/com/siriusxi/javamag/kata/RomanNumeralsConverterTests.java
+++ RomanNumerals/src/test/java/com/siriusxi/javamag/kata/RomanNumeralsConverterTests.java
@@ -17,7 +17,7 @@ public class RomanNumeralsConverterTests {
         assertEquals(1, arabic);
     }
 
-    private int convert(String romanNumeral) {
+    private static int convert(String romanNumeral) {
         return 1;
     }
 }
