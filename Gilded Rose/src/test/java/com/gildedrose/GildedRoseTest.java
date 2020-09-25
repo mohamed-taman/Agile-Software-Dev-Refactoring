7f12231d5ea32b4d8fa3e0b2d3dc8c89a2a48884
Mohamed Taman <mohamed.taman@gmail.com>
2020-09-25 19:59:34 +0200

    Step #A2-4: Adding more pin-down tests

--- Gilded Rose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Gilded Rose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,20 +6,36 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
-    @Test
-    void junitFrameworkWorks() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("foo", app.items[0].name);
-    }
+  @Test
+  void junitFrameworkWorks() {
+    Item[] items = new Item[] {new Item("foo", 0, 0)};
+    GildedRose app = new GildedRose(items);
+    app.updateQuality();
+    assertEquals("foo", app.items[0].name);
+  }
 
-    @Test
-    void systemLowersValues() {
-        Item[] items = new Item[] { new Item("foo", 15, 25)};
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals(14, app.items[0].sellIn);
-        assertEquals(24, app.items[0].quality);
-    }
+  @Test
+  void systemLowersValues() {
+    Item[] items = new Item[] {new Item("foo", 15, 25)};
+    GildedRose app = new GildedRose(items);
+    app.updateQuality();
+    assertEquals(14, app.items[0].sellIn);
+    assertEquals(24, app.items[0].quality);
+  }
+
+  @Test
+  void qualityDegradesTwiceAsFast() {
+    Item[] items = new Item[] {new Item("foo", 0, 17)};
+    GildedRose app = new GildedRose(items);
+    app.updateQuality();
+    assertEquals(15, app.items[0].quality);
+  }
+
+  @Test
+  void qualityIsNeverNegative() {
+    Item[] items = new Item[] {new Item("foo", 0, 0)};
+    GildedRose app = new GildedRose(items);
+    app.updateQuality();
+    assertEquals(0, app.items[0].quality);
+  }
 }
