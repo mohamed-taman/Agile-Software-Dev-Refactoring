595886adaf0874315dcb45115035163f792c9b29
Mohamed Taman <mohamed.taman@gmail.com>
2020-09-26 00:02:54 +0200

    Step #A2-6: Refactoring: Change Method Signature

--- Gilded Rose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Gilded Rose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,8 +6,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
-  private Item createAndUpdate(int sellin, int quality) {
-    Item[] items = new Item[] {new Item("foo", sellin, quality)};
+  private Item createAndUpdate(String name, int sellin, int quality) {
+    Item[] items = new Item[] {new Item(name, sellin, quality)};
     GildedRose app = new GildedRose(items);
     app.updateQuality();
     return items[0];
@@ -15,26 +15,32 @@ class GildedRoseTest {
 
   @Test
   void junitFrameworkWorks() {
-    Item item = createAndUpdate(0, 0);
+    Item item = createAndUpdate("foo", 0, 0);
     assertEquals("foo", item.name);
   }
 
   @Test
   void systemLowersValues() {
-    Item item = createAndUpdate(15, 25);
+    Item item = createAndUpdate("foo", 15, 25);
     assertEquals(14, item.sellIn);
     assertEquals(24, item.quality);
   }
 
   @Test
   void qualityDegradesTwiceAsFast() {
-    Item item = createAndUpdate(0, 17);
+    Item item = createAndUpdate("foo", 0, 17);
     assertEquals(15, item.quality);
   }
 
   @Test
   void qualityIsNeverNegative() {
-    Item item = createAndUpdate(0, 0);
+    Item item = createAndUpdate("foo", 0, 0);
     assertEquals(0, item.quality);
+  }
+
+  @Test
+  void agedBrieIncreasesInQuality() {
+    Item item = createAndUpdate("Aged Brie", 15, 25);
+    assertEquals(26, item.quality);
   }
 }
