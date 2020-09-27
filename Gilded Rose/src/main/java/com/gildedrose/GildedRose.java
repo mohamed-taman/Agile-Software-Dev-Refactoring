ada3508d3fc46a0df1153c43645acf04cc638cb6
Mohamed Taman <mohamed.taman@gmail.com>
2020-09-27 23:08:41 +0200

    Step #A3-1: Refactoring production code; Extract String Constants

--- Gilded Rose/src/main/java/com/gildedrose/GildedRose.java
+++ Gilded Rose/src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,9 @@
 package com.gildedrose;
 
 class GildedRose {
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -9,10 +12,10 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (!items[i].name.equals(AGED_BRIE)
+                    && !items[i].name.equals(BACKSTAGE_PASSES)) {
                 if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                    if (!items[i].name.equals(SULFURAS)) {
                         items[i].quality = items[i].quality - 1;
                     }
                 }
@@ -20,7 +23,7 @@ class GildedRose {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (items[i].name.equals(BACKSTAGE_PASSES)) {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
@@ -36,15 +39,15 @@ class GildedRose {
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (!items[i].name.equals(SULFURAS)) {
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (!items[i].name.equals(AGED_BRIE)) {
+                    if (!items[i].name.equals(BACKSTAGE_PASSES)) {
                         if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                            if (!items[i].name.equals(SULFURAS)) {
                                 items[i].quality = items[i].quality - 1;
                             }
                         }
--- Gilded Rose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Gilded Rose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,9 +2,12 @@ package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
 
+import static com.gildedrose.GildedRose.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
+
+  private final String FOO = "foo";
 
   private Item createAndUpdate(String name, int sellin, int quality) {
     Item[] items = new Item[] {new Item(name, sellin, quality)};
@@ -15,106 +18,106 @@ class GildedRoseTest {
 
   @Test
   void junitFrameworkWorks() {
-    Item item = createAndUpdate("foo", 0, 0);
-    assertEquals("foo", item.name);
+    Item item = createAndUpdate(FOO, 0, 0);
+    assertEquals(FOO, item.name);
   }
 
   @Test
   void systemLowersValues() {
-    Item item = createAndUpdate("foo", 15, 25);
+    Item item = createAndUpdate(FOO, 15, 25);
     assertEquals(14, item.sellIn);
     assertEquals(24, item.quality);
   }
 
   @Test
   void qualityDegradesTwiceAsFast() {
-    Item item = createAndUpdate("foo", 0, 17);
+    Item item = createAndUpdate(FOO, 0, 17);
     assertEquals(15, item.quality);
   }
 
   @Test
   void qualityIsNeverNegative() {
-    Item item = createAndUpdate("foo", 0, 0);
+    Item item = createAndUpdate(FOO, 0, 0);
     assertEquals(0, item.quality);
   }
 
   @Test
   void agedBrieIncreasesInQuality() {
-    Item item = createAndUpdate("Aged Brie", 15, 25);
+    Item item = createAndUpdate(AGED_BRIE, 15, 25);
     assertEquals(26, item.quality);
   }
 
   @Test
   void qualityNeverMoreThan50() {
-    Item item = createAndUpdate("foo", 15, 52);
+    Item item = createAndUpdate(FOO, 15, 52);
     assertEquals(51, item.quality); // can exceed 50 if input exceeds 50
 
-    item = createAndUpdate("Aged Brie", 15, 50);
+    item = createAndUpdate(AGED_BRIE, 15, 50);
     assertEquals(50, item.quality);
   }
 
   @Test
   void sulfurasNeverHasToBeSoldOrDecreasesInQuality() {
-    Item item = createAndUpdate("Sulfuras, Hand of Ragnaros", 1, 42);
+    Item item = createAndUpdate(SULFURAS, 1, 42);
     assertEquals(1, item.sellIn); // sellIn doesn't change
     assertEquals(42, item.quality); // quality doesn't change
   }
 
   @Test
   void backstagePassesIncreaseInQuality() {
-    Item item = createAndUpdate("Backstage passes to a TAFKAL80ETC concert", 15, 25);
+    Item item = createAndUpdate(BACKSTAGE_PASSES, 15, 25);
     assertEquals(26, item.quality);
   }
 
   @Test
   void backstagePassesIncreaseBy2() {
-    Item item = createAndUpdate("Backstage passes to a TAFKAL80ETC concert", 10, 25);
+    Item item = createAndUpdate(BACKSTAGE_PASSES, 10, 25);
     assertEquals(27, item.quality);
   }
 
   @Test
   void backstagePassesIncreaseBy3() {
-    Item item = createAndUpdate("Backstage passes to a TAFKAL80ETC concert", 5, 25);
+    Item item = createAndUpdate(BACKSTAGE_PASSES, 5, 25);
     assertEquals(28, item.quality);
   }
 
   @Test
   void backstagePassesQualityDropTo0() {
-    Item item = createAndUpdate("Backstage passes to a TAFKAL80ETC concert", 0, 25);
+    Item item = createAndUpdate(BACKSTAGE_PASSES, 0, 25);
     assertEquals(0, item.quality);
   }
 
   @Test
   void agedBrieNeverExpires() {
-    Item item = createAndUpdate("Aged Brie", 0, 42);
+    Item item = createAndUpdate(AGED_BRIE, 0, 42);
     assertEquals(-1, item.sellIn);
     assertEquals(44, item.quality);
   }
 
   @Test
   void backstagePassMaximumQuality() {
-    Item item = createAndUpdate("Backstage passes to a TAFKAL80ETC concert", 10, 48);
+    Item item = createAndUpdate(BACKSTAGE_PASSES, 10, 48);
     assertEquals(50, item.quality);
 
-    item = createAndUpdate("Backstage passes to a TAFKAL80ETC concert", 10, 49);
+    item = createAndUpdate(BACKSTAGE_PASSES, 10, 49);
     assertEquals(50, item.quality);
 
-    item = createAndUpdate("Backstage passes to a TAFKAL80ETC concert", 5, 49);
+    item = createAndUpdate(BACKSTAGE_PASSES, 5, 49);
     assertEquals(50, item.quality);
   }
 
   @Test
   void degradeInQualityUnlessSulfuras() {
-    Item item = createAndUpdate("foo", -1, 1);
+    Item item = createAndUpdate(FOO, -1, 1);
     assertEquals(0, item.quality);
 
-    item = createAndUpdate("Sulfuras, Hand of Ragnaros", -1, 1);
+    item = createAndUpdate(SULFURAS, -1, 1);
     assertEquals(1, item.quality);
   }
 
   @Test
   void agedBrieMaximumQuality() {
-    Item item = createAndUpdate("Aged Brie", -1, 49);
+    Item item = createAndUpdate(AGED_BRIE, -1, 49);
     assertEquals(50, item.quality);
   }
 }
