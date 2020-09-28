de403c01eae961926d48a6f9aaa98fc971b7f783
Mohamed Taman <mohamed.taman@gmail.com>
2020-09-28 14:55:44 +0200

    Step #A3-3: Extract Variable

--- Gilded Rose/src/main/java/com/gildedrose/GildedRose.java
+++ Gilded Rose/src/main/java/com/gildedrose/GildedRose.java
@@ -15,51 +15,52 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals(AGED_BRIE)
-                    && !items[i].name.equals(BACKSTAGE_PASSES)) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals(SULFURAS)) {
-                        items[i].quality = items[i].quality - 1;
+            final Item item = items[i];
+            if (!item.name.equals(AGED_BRIE)
+                    && !item.name.equals(BACKSTAGE_PASSES)) {
+                if (item.quality > 0) {
+                    if (!item.name.equals(SULFURAS)) {
+                        item.quality = item.quality - 1;
                     }
                 }
             } else {
-                if (items[i].quality < MAXIMUM_QUALITY) {
-                    items[i].quality = items[i].quality + 1;
+                if (item.quality < MAXIMUM_QUALITY) {
+                    item.quality = item.quality + 1;
 
-                    if (items[i].name.equals(BACKSTAGE_PASSES)) {
-                        if (items[i].sellIn < BACKSTAGE_PASS_THRESHOLD1) {
-                            if (items[i].quality < MAXIMUM_QUALITY) {
-                                items[i].quality = items[i].quality + 1;
+                    if (item.name.equals(BACKSTAGE_PASSES)) {
+                        if (item.sellIn < BACKSTAGE_PASS_THRESHOLD1) {
+                            if (item.quality < MAXIMUM_QUALITY) {
+                                item.quality = item.quality + 1;
                             }
                         }
 
-                        if (items[i].sellIn < BACKSTAGE_PASS_THRESHOLD2) {
-                            if (items[i].quality < MAXIMUM_QUALITY) {
-                                items[i].quality = items[i].quality + 1;
+                        if (item.sellIn < BACKSTAGE_PASS_THRESHOLD2) {
+                            if (item.quality < MAXIMUM_QUALITY) {
+                                item.quality = item.quality + 1;
                             }
                         }
                     }
                 }
             }
 
-            if (!items[i].name.equals(SULFURAS)) {
-                items[i].sellIn = items[i].sellIn - 1;
+            if (!item.name.equals(SULFURAS)) {
+                item.sellIn = item.sellIn - 1;
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals(AGED_BRIE)) {
-                    if (!items[i].name.equals(BACKSTAGE_PASSES)) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals(SULFURAS)) {
-                                items[i].quality = items[i].quality - 1;
+            if (item.sellIn < 0) {
+                if (!item.name.equals(AGED_BRIE)) {
+                    if (!item.name.equals(BACKSTAGE_PASSES)) {
+                        if (item.quality > 0) {
+                            if (!item.name.equals(SULFURAS)) {
+                                item.quality = item.quality - 1;
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        item.quality = item.quality - item.quality;
                     }
                 } else {
-                    if (items[i].quality < MAXIMUM_QUALITY) {
-                        items[i].quality = items[i].quality + 1;
+                    if (item.quality < MAXIMUM_QUALITY) {
+                        item.quality = item.quality + 1;
                     }
                 }
             }
